7282518f03b058048ce15432fc2990dbde776652
dbads <deepakbharti823@gmail.com>
2021-07-03 21:10:56 +0530

    fixed issue comment button not being enabled

--- content.js
+++ content.js
@@ -1,20 +1,39 @@
 console.log('hello there')
 
-window.onload = function() {
-
+function insertTriggerButton() {
     let but = document.createElement("button")
-    but.innerText = "Retrigger"
+    but.innerText = "Run the pipeline"
     but.style.background="green"
+    but.style.position="fixed"
+    but.style.borderRadius="50px"
+    but.style.borderColor="#23272C"
+    but.style.zIndex="#23272C"
+    but.style.marginTop="50px"
     but.setAttribute("id", "trigger")
     document.getElementsByClassName("gh-header-actions")[0].appendChild(but)
+    console.log('added button ----');
+}
 
-    console.log('done ----');
+function addEventListenerToTriggerButton() {
+    let commentButton = document.evaluate('//*[@id="partial-new-comment-form-actions"]/div/div[2]/button', 
+    document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 
     document.getElementById("trigger").addEventListener("click", function() {
         console.log("triggered ---, wuhooo");
-        document.getElementById("new_comment_field").innerText="retest this please"
-        document.getElementsByClassName("color-bg-secondary ml-1")[0].disabled = null;
-        document.getElementsByClassName("color-bg-secondary ml-1")[0].click();
-
+        document.getElementById("new_comment_field").value="retest this please"; // type retest this please in the comment box
+        commentButton.disabled=null; // enable the comment button
+        commentButton.click(); // click the comment button
+        console.log('submitted--');
     })
+
+    console.log('added evetn listener')
 }
+
+function insertButtonAndTriggerPipeline() {
+    insertTriggerButton()
+    addEventListenerToTriggerButton()
+}
+
+insertButtonAndTriggerPipeline()
+
+// Autofocus processing was blocked because a document already has a focused element.
